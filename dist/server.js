a62e20ea5b95e11da98b887e54dc279a8f9a65f5
Настя <ablovatskaja@gmail.com>
2022-10-23 19:18:46 +0300

    change deploy

--- dist/server.js
+++ dist/server.js
@@ -76,6 +76,7 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)());
 app.use("/api", authRouter_1.authRouter);
+app.get("/", function (req, res) { return res.json("hi"); });
 app.use(errorMiddleware_1.ErrorMiddleware);
 var start = function () { return __awaiter(void 0, void 0, void 0, function () {
     var e_1;
