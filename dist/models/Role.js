4409a44d2cb1878f472707f3febd38422d5f4b6b
Настя <ablovatskaja@gmail.com>
2022-10-23 17:39:00 +0300

    rebuild dist

--- dist/models/Role.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleModel = exports.Role = void 0;
-var mongoose_1 = require("mongoose");
-exports.Role = new mongoose_1.Schema({
-    value: { type: String, unique: true, default: "USER" },
-});
-exports.RoleModel = (0, mongoose_1.model)("Role", exports.Role);
-//# sourceMappingURL=Role.js.map
--- dist/models/Status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StatusModel = exports.Status = void 0;
-var mongoose_1 = require("mongoose");
-exports.Status = new mongoose_1.Schema({
-    value: { type: String, unique: true, default: "active" },
-});
-exports.StatusModel = (0, mongoose_1.model)("Status", exports.Status);
-//# sourceMappingURL=Status.js.map
