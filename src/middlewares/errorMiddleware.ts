59708815b7cc64449827e0b7680ad7999b3013c6
Настя <ablovatskaja@gmail.com>
2022-10-23 17:16:54 +0300

    fix errors

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -2,7 +2,7 @@ import { ApiError } from "./../exceptions/apiError";
 
 export const ErrorMiddleware = (err, req, res, next) => {
   console.log(err);
-  if (err instanceof ApiError) {
+  if (err) {
     return res
       .status(err.status)
       .json({ message: err.message, errors: err.errors });
