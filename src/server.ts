703c5d5affa04e2a450a1e0bc32a3871bb507ca2
Настя <ablovatskaja@gmail.com>
2022-10-23 19:13:55 +0300

    change server

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use("/api", authRouter);
+app.get("/", (req, res) => res.json("hi"));
+
 app.use(ErrorMiddleware);
 
 const start = async () => {
