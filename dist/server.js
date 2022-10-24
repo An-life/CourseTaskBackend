33bf60b7f2a76414f86e402f8a70adadd1f57813
Настя <ablovatskaja@gmail.com>
2022-10-24 10:30:12 +0300

    add client url

--- dist/server.js
+++ dist/server.js
@@ -71,10 +71,13 @@ var authRouter_1 = require("./router/authRouter");
 var errorMiddleware_1 = require("./middlewares/errorMiddleware");
 dotenv.config();
 var app = (0, express_1.default)();
-var PORT = 5000;
+var PORT = process.env.PORT || 5000;
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
 app.use("/api", authRouter_1.authRouter);
 app.get("/", function (req, res) { return res.json("hi"); });
 app.use(errorMiddleware_1.ErrorMiddleware);
